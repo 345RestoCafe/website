86435babf5bcdedcc784843878aaa390e8488123
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2025-02-16 21:29:41 -0500

    update(): HeaderComponent

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './public/router'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
-import './assets/styles/custom.scss';
+import './assets/styles/custom.scss'
 
 createApp(App)
     .use(router)
--- src/public/components/HeaderComponent.vue
+++ src/public/components/HeaderComponent.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="header-container">
-    <div class="content">
-      <div class="contact-numbers">
-        <p class="info">Resto Café, Bar y Delivery</p>
-        <p class="phone">+51 993 709 111 &nbsp;</p>
-        <p class="info">/</p>
-        <p class="info">Eventos</p>
-        <p class="phone">+51 987 955 926</p>
-      </div>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container text-white">
+      <div class="row w-100 align-items-center">
+        <!-- Información de contacto -->
+        <div class="col-md-6 d-flex align-items-center justify-content-center">
+          <p class="info mb-0 mt-0 m-2">Resto Café, Bar y Delivery</p>
+          <p class="phone mb-0 mt-0 m-2">+51 993 709 111</p>
+          <p class="info mb-0 mt-0 m-2">/</p>
+          <p class="info mb-0 mt-0 m-2">Eventos</p>
+          <p class="phone mb-0 mt-0 m-2">+51 987 955 926</p>
+        </div>
 
-      <div class="mail-container">
-        <i class="pi pi-envelope" style="font-size: 1rem"></i>
-        <strong class="mail">&nbsp;&nbsp;contacto@345restocafe.com</strong>
-        <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>
-        <i class="pi pi-envelope" style="font-size: 1rem"></i>
-        <strong class="mail">&nbsp;&nbsp;eventos@345retocafe.com</strong>
-      </div>
+        <!-- Correos electrónicos -->
+        <div class="col-md-2 d-flex align-items-center justify-content-center">
+          <i class="pi pi-envelope me-2" style="font-size: 1rem"></i>
+          <p class="mail mb-0">contacto@345restocafe.com</p>
+        </div>
+        <div class="col-md-2 d-flex align-items-center justify-content-center">
+          <i class="pi pi-envelope me-2" style="font-size: 1rem"></i>
+          <p class="mail mb-0">eventos@345retocafe.com</p>
+        </div>
 
-      <div class="social-media-container">
-        <a class="pi pi-facebook" style="font-size: 1rem" href="https://www.facebook.com/profile.php?id=100092175221621"/>
-        <a class="pi pi-instagram" style="font-size: 1rem" href="https://www.instagram.com/345restocafe/"/>
-        <a class="pi pi-tiktok" style="font-size: 1rem" href="https://www.tiktok.com/@345restocafe"/>
+        <!-- Redes sociales -->
+        <div class="col-md-2 d-flex align-items-center justify-content-center">
+          <a class="pi pi-facebook text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.facebook.com/profile.php?id=100092175221621"></a>
+          <a class="pi pi-instagram text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.instagram.com/345restocafe/"></a>
+          <a class="pi pi-tiktok text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.tiktok.com/@345restocafe"></a>
+        </div>
       </div>
     </div>
-  </div>
+  </nav>
 </template>
 
 <script>
@@ -39,71 +45,18 @@ export default {
 </script>
 
 <style scoped>
-.header-container{
-  top: 0;
-  left: 0;
-  position: absolute;
-  width: 100%;
-  background-color: #000;
-  color: #fff;
-  font-family: "Poppins", sans-serif;
+p {
+  font-family: "Popins", sans-serif;
   font-size: 0.8em;
   font-weight: 400;
-  display: flex;
-  justify-content: center;
-  padding: 4px 0;
-  transition: top 0.3s;
+  line-height: 32px;
 }
-.header-container.hidden {
-  top: -100px;
+.phone {
+  font-weight: 800;
+  letter-spacing: -1px;
 }
-.content {
-  display: flex;
-  justify-content: space-between;
-}
-
-.contact-numbers{
-  display: flex;
-}
-.info{
-  padding: 0 10px;
-}
-.phone{
-  font-weight: bold;
-}
-
-.mail-container{
-  align-self: center;
-  padding: 0 50px;
-  display: flex;
-}
-
-.social-media-container{
-  align-self: center;
-}
-a {
-  color: #fff;
-  text-decoration: none;
-  padding: 0 5px;
-}
-
-@media (max-width: 1125px) {
-  .header-container{
-    font-size: 0.6em;
-  }
-  .content{
-    flex-direction: column;
-    padding: 5px;
-  }
-  .mail-container{
-    padding: 10px 0;
-  }
-}
-
-@media (max-width: 370px) {
-  .header-container{
-    font-size: 0.45em;
-  }
-
+.mail {
+  font-weight: 600;
+  font-size: 0.85em;
 }
 </style>
