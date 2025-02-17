2df5d57e8028cb0b64661f994aeacfb61513d923
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2025-02-16 22:01:28 -0500

    fix(): Header
    Intregracion con bootstrap finalizada

--- src/public/components/HeaderComponent.vue
+++ src/public/components/HeaderComponent.vue
@@ -1,31 +1,31 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <div class="container text-white">
+    <div class="container text-white  justify-content-center">
       <div class="row w-100 align-items-center">
         <!-- Información de contacto -->
-        <div class="col-md-6 d-flex align-items-center justify-content-center">
-          <p class="info mb-0 mt-0 m-2">Resto Café, Bar y Delivery</p>
-          <p class="phone mb-0 mt-0 m-2">+51 993 709 111</p>
-          <p class="info mb-0 mt-0 m-2">/</p>
-          <p class="info mb-0 mt-0 m-2">Eventos</p>
-          <p class="phone mb-0 mt-0 m-2">+51 987 955 926</p>
+        <div class="col-lg-6 d-flex align-items-center justify-content-center flex-wrap">
+          <p class="info mb-0 me-2">Resto Café, Bar y Delivery</p>
+          <p class="phone mb-0 me-2">+51 993 709 111</p>
+          <p class="info mb-0 me-2">/</p>
+          <p class="info mb-0 me-2">Eventos</p>
+          <p class="phone mb-0">+51 987 955 926</p>
         </div>
 
         <!-- Correos electrónicos -->
-        <div class="col-md-2 d-flex align-items-center justify-content-center">
+        <div class="col-lg-2 d-flex align-items-center justify-content-center">
           <i class="pi pi-envelope me-2" style="font-size: 1rem"></i>
           <p class="mail mb-0">contacto@345restocafe.com</p>
         </div>
-        <div class="col-md-2 d-flex align-items-center justify-content-center">
+        <div class="col-lg-2 d-flex align-items-center justify-content-center">
           <i class="pi pi-envelope me-2" style="font-size: 1rem"></i>
           <p class="mail mb-0">eventos@345retocafe.com</p>
         </div>
 
         <!-- Redes sociales -->
-        <div class="col-md-2 d-flex align-items-center justify-content-center">
-          <a class="pi pi-facebook text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.facebook.com/profile.php?id=100092175221621"></a>
-          <a class="pi pi-instagram text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.instagram.com/345restocafe/"></a>
-          <a class="pi pi-tiktok text-white m-2" style="font-size: 1rem; text-decoration: none;" href="https://www.tiktok.com/@345restocafe"></a>
+        <div class="col-lg-2 d-flex align-items-center justify-content-center">
+          <a class="pi pi-facebook text-white me-3" style="font-size: 1rem; text-decoration: none;" href="https://www.facebook.com/profile.php?id=100092175221621"></a>
+          <a class="pi pi-instagram text-white me-3" style="font-size: 1rem; text-decoration: none;" href="https://www.instagram.com/345restocafe/"></a>
+          <a class="pi pi-tiktok text-white me-3" style="font-size: 1rem; text-decoration: none;" href="https://www.tiktok.com/@345restocafe"></a>
         </div>
       </div>
     </div>
@@ -45,12 +45,21 @@ export default {
 </script>
 
 <style scoped>
+
 p {
   font-family: "Popins", sans-serif;
   font-size: 0.8em;
   font-weight: 400;
   line-height: 32px;
 }
+
+.info,
+.phone,
+.mail {
+  margin: 0;
+  white-space: nowrap;
+}
+
 .phone {
   font-weight: 800;
   letter-spacing: -1px;
@@ -58,5 +67,17 @@ p {
 .mail {
   font-weight: 600;
   font-size: 0.85em;
+}
+
+@media (max-width: 1400px) {
+  .info, .phone, .mail {
+    font-size: 0.7rem;
+  }
+}
+
+@media (max-width: 1200px) {
+  .info, .phone, .mail {
+    font-size: 0.6rem;
+  }
 }
 </style>
